148a80217c53ea5dc2938598887633ab7c3db36d
Ollie Wagner <ollie@yapstudios.com>
2026-01-30 11:09:35 -0700

    Point formula at homebrew-tap releases (public) (#2)

--- Formula/metabind.rb
+++ Formula/metabind.rb
@@ -6,12 +6,10 @@ class Metabind < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/yapstudios/metabind-cli/releases/download/v0.1.0/mb-darwin-arm64.tar.gz",
-          using: GitHubPrivateRepositoryReleaseDownloadStrategy
+      url "https://github.com/yapstudios/homebrew-tap/releases/download/v0.1.0/mb-darwin-arm64.tar.gz"
       sha256 "0241ab4b55be17788ca4fc9179462e0c00bd768c10e6b9d7ecbd4cc2ec6b5cc8"
     else
-      url "https://github.com/yapstudios/metabind-cli/releases/download/v0.1.0/mb-darwin-x64.tar.gz",
-          using: GitHubPrivateRepositoryReleaseDownloadStrategy
+      url "https://github.com/yapstudios/homebrew-tap/releases/download/v0.1.0/mb-darwin-x64.tar.gz"
       sha256 "4011b0b9452b567c571f5a878326700e4cf22991d55d4b0771342c2c43133d59"
     end
   end
