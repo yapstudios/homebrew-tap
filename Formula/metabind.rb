2c6bd20d101ac0d19ebf33caeb9fb351dd7bf409
Ollie Wagner <ollie@yapstudios.com>
2026-01-30 11:06:00 -0700

    Use GitHubPrivateRepositoryReleaseDownloadStrategy for private repo (#1)

--- Formula/metabind.rb
+++ Formula/metabind.rb
@@ -6,10 +6,12 @@ class Metabind < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/yapstudios/metabind-cli/releases/download/v0.1.0/mb-darwin-arm64.tar.gz"
+      url "https://github.com/yapstudios/metabind-cli/releases/download/v0.1.0/mb-darwin-arm64.tar.gz",
+          using: GitHubPrivateRepositoryReleaseDownloadStrategy
       sha256 "0241ab4b55be17788ca4fc9179462e0c00bd768c10e6b9d7ecbd4cc2ec6b5cc8"
     else
-      url "https://github.com/yapstudios/metabind-cli/releases/download/v0.1.0/mb-darwin-x64.tar.gz"
+      url "https://github.com/yapstudios/metabind-cli/releases/download/v0.1.0/mb-darwin-x64.tar.gz",
+          using: GitHubPrivateRepositoryReleaseDownloadStrategy
       sha256 "4011b0b9452b567c571f5a878326700e4cf22991d55d4b0771342c2c43133d59"
     end
   end
