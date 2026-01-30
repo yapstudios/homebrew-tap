5d670935ff7b5428592a47da04099144dc431bd1
Ollie Wagner <ollie@yapstudios.com>
2026-01-30 16:33:28 -0700

    Update metabind to 0.4.1 (#7)
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- Formula/metabind.rb
+++ Formula/metabind.rb
@@ -1,16 +1,16 @@
 class Metabind < Formula
   desc "Metabind CLI — headless CMS management from the command line"
   homepage "https://github.com/yapstudios/metabind-cli"
-  version "0.4.0"
+  version "0.4.1"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/yapstudios/homebrew-tap/releases/download/v0.4.0/mb-darwin-arm64.tar.gz"
-      sha256 "b7b72dd9a637bc09747194af26f1b45c04930e4ec6899c16da5bfd3de173dc93"
+      url "https://github.com/yapstudios/homebrew-tap/releases/download/v0.4.1/mb-darwin-arm64.tar.gz"
+      sha256 "d7ca9024e0db001683413257ac77774fa3bd06814e9d8de3ba9731d94d5b1211"
     else
-      url "https://github.com/yapstudios/homebrew-tap/releases/download/v0.4.0/mb-darwin-x64.tar.gz"
-      sha256 "5c1bc67332319df2681a2e6215dae91e2a0b02e172699cc830fa08ff2b829e3e"
+      url "https://github.com/yapstudios/homebrew-tap/releases/download/v0.4.1/mb-darwin-x64.tar.gz"
+      sha256 "8f576395a3b0208b693e0d89459b99dabd3a8a879aca07219336ac412961297b"
     end
   end
 
@@ -23,6 +23,6 @@ class Metabind < Formula
   end
 
   test do
-    assert_match "0.4.0", shell_output("#{bin}/metabind --version")
+    assert_match "0.4.1", shell_output("#{bin}/metabind --version")
   end
 end
