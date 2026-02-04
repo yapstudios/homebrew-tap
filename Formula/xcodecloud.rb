19e6670879ffc32ceeacae8ed552892d8ad1ecb9
Emory Al-Imam <emory@yapstudios.com>
2026-02-04 15:29:42 -0800

    Update xcodecloud to 1.1.3

--- Formula/xcodecloud.rb
+++ Formula/xcodecloud.rb
@@ -1,8 +1,8 @@
 class Xcodecloud < Formula
   desc "Command-line interface for Xcode Cloud via App Store Connect API"
   homepage "https://github.com/yapstudios/xcodecloud-cli"
-  url "https://github.com/yapstudios/xcodecloud-cli/archive/refs/tags/1.1.2.tar.gz"
-  sha256 "7f9743821ba11ea4f95b5107a35c0bcf99b606835d6369cb7a3c93800fb3d7eb"
+  url "https://github.com/yapstudios/xcodecloud-cli/archive/refs/tags/1.1.3.tar.gz"
+  sha256 "141dabf85eb0935fda7e89c245f0c1eb5612260307076b7a68b7169656bcbeb6"
   license "MIT"
 
   depends_on xcode: ["16.0", :build]
