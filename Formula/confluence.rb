47e8d7746714c0ab87e0c92e6d610fcacbc4b767
Ollie Wagner <ollie@yapstudios.com>
2026-02-18 12:55:09 -0700

    Relax credential test for corporate proxies

--- Formula/confluence.rb
+++ Formula/confluence.rb
@@ -7,10 +7,10 @@ class Confluence < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/yapstudios/homebrew-tap/releases/download/confluence-v0.1.0/confluence-darwin-arm64.tar.gz"
-      sha256 "22d834b3bef3a358328394a3b56a7a71e44bb2cd43c60f1d61c9a4fd08255a4e"
+      sha256 "3e004883c0e6256fbd0ad6dd8f798fd1c444874b6c5f120b94a872f2456309fd"
     else
       url "https://github.com/yapstudios/homebrew-tap/releases/download/confluence-v0.1.0/confluence-darwin-x64.tar.gz"
-      sha256 "a9e910a3d4864e7592038f52ccd809eced0f7c9e77a1ec5d48a2b3d37a4d74d7"
+      sha256 "28cea5a613dabd08470aadc07998375b4c5e8ab1392555416fcb0519bd20c6be"
     end
   end
 
