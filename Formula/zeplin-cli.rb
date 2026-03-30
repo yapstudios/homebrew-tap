ad1dfdb81fd2c9e4c820230260da721e6567a128
Emory Al-Imam <emory@yapstudios.com>
2026-03-30 14:26:59 -0700

    Update zeplin-cli to 0.5.0 with prebuilt binary

--- Formula/zeplin-cli.rb
+++ Formula/zeplin-cli.rb
@@ -1,16 +1,14 @@
 class ZeplinCli < Formula
   desc "Command-line interface for the Zeplin API"
   homepage "https://github.com/yapstudios/zeplin-cli"
-  url "https://github.com/yapstudios/zeplin-cli/archive/refs/tags/0.4.3.tar.gz"
-  sha256 "f72257826f992b3abb6e7a31370c4295bbba0417b6c7797fde07b5d9020ca444"
+  url "https://github.com/yapstudios/zeplin-cli/releases/download/0.5.0/zeplin-cli-macos.tar.gz"
+  sha256 "681cdfbaa20709669fd9f730f0837914ab384207bf90a48038cc9b5963e86d96"
   license "MIT"
 
-  depends_on xcode: ["16.0", :build]
   depends_on macos: :monterey
 
   def install
-    system "swift", "build", "-c", "release", "--disable-sandbox"
-    bin.install ".build/release/zeplin-cli"
+    bin.install "zeplin-cli"
     generate_completions_from_executable(bin/"zeplin-cli", "--generate-completion-script")
   end
 
