919be8df45ca7e04f30666792318a3477e9fdacd
Ollie Wagner <ollie@yapstudios.com>
2026-05-12 14:57:54 -0600

    Fix sfsym formula audit

--- Formula/sfsym.rb
+++ Formula/sfsym.rb
@@ -4,7 +4,6 @@ class Sfsym < Formula
   url "https://github.com/yapstudios/sfsym/releases/download/v0.2.11/sfsym-v0.2.11-macos-universal.tar.gz"
   sha256 "0d34f1233e48b695a4435533436a51a296f74bd9e673b09f1a243f628a3fb590"
   license "MIT"
-  version "0.2.11"
   depends_on :macos
   def install
     bin.install "sfsym"
